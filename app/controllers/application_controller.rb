088e2175884f535e368e5197083207c42997900d
djShrek <boho527@gmail.com>
2016-03-07 00:30:08 -0800

    add jsonP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
     @customers = Customer.all
                   .where(web_page_viewable: true)
                   .map { |c| [c.note, x.gallery_web_address] }
-    render json: @customers
+    render json: @customers.to_json, callback: params[:something]
   end
 end
