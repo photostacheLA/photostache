63e1c91c74963b020eb48fed75bbe95f95b028f8
djShrek <boho527@gmail.com>
2016-03-08 23:11:32 -0800

    remove protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  protect_from_forgery except: :gallery_web_address
 
   def gallery_web_address
     @customers = Customer.all
                   .where(web_page_viewable: true)
                   .map { |c| [c.note, c.gallery_web_address] }
+
     render json: @customers, callback: params['callback']
   end
+
 end
